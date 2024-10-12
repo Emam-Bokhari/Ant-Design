81ca908c30a26d57304855c29d3dd7de4746b08b
Moshfiqur Rahman <studentemam@gmail.com>
2024-10-12 20:50:16 +0600

    init: initialize project setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Fragment } from "react";
+
+export default function App(){
+  return(
+    <Fragment>
+      <div>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptates neque quibusdam a adipisci distinctio temporibus eius eum veniam repellendus. Eligendi totam ut, pariatur illum laboriosam distinctio delectus vitae quidem earum?</p>
+      </div>
+      
+    </Fragment>
+  )
+}
