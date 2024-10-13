90dcbbbdb7512d4d17830e7a943331483cfebb84
Moshfiqur Rahman <studentemam@gmail.com>
2024-10-13 11:09:46 +0600

    feat(ButtonComponent): add buttons with different varaints and color options

--- src/components/learning/general/ButtonComponent.jsx
+++ src/components/learning/general/ButtonComponent.jsx
@@ -10,6 +10,26 @@ export default function ButtonComponent() {
       <Button type="link">Link</Button>
       <Button type="primary">Primary</Button>
       <Button type="text">Text</Button>
+      <hr style={{ margin: "16px 0" }} />
+      {/* variant and color */}
+      <Button color="primary" variant="solid">
+        Solid
+      </Button>
+      <Button color="danger" variant="dashed">
+        Dashed
+      </Button>
+      <Button color="primary" variant="filled">
+        Filled
+      </Button>
+      <Button color="danger" variant="link">
+        Link
+      </Button>
+      <Button color="primary" variant="outlined">
+        Outlined
+      </Button>
+      <Button color="danger" variant="text">
+        Text
+      </Button>
     </Fragment>
   );
 }
