19c8d39641e3f83563eea0fc50be5e20a2f8180d
Moshfiqur Rahman <studentemam@gmail.com>
2024-10-19 10:33:32 +0600

    feat(Breadcrumb): configure separator for breadcrumb component

--- src/components/learning/navigation/BreadcrumbComponent.jsx
+++ src/components/learning/navigation/BreadcrumbComponent.jsx
@@ -13,6 +13,22 @@ export default function BreadcrumbComponent() {
           { title: "An Application" },
         ]}
       />
+      {/* config the seperator */}
+      <Breadcrumb
+        separator=">"
+        items={[
+          {
+            title: "Home",
+          },
+          {
+            title: <a href="#">Application Center</a>,
+          },
+          {
+            title: <a href="#">Application List</a>,
+          },
+          { title: "An Application" },
+        ]}
+      />
     </Fragment>
   );
 }
