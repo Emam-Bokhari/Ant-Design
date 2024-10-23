3090d66cb22e7f358511f6af6b32f9a15f211de9
Moshfiqur Rahman <studentemam@gmail.com>
2024-10-23 13:12:22 +0600

    feat(Input): add size variants to Input component

--- src/components/learning/date-entry/InputComponent.jsx
+++ src/components/learning/date-entry/InputComponent.jsx
@@ -1,10 +1,14 @@
 import { Input } from "antd";
 import { Fragment } from "react";
 
-export default function InputComponent(){
-    return(
-        <Fragment>
-            <Input placeholder="Enter Your Name " />
-        </Fragment>
-    )
+export default function InputComponent() {
+  return (
+    <Fragment>
+      <Input placeholder="Enter Your Name " />
+      {/* size */}
+      <Input placeholder="small" size="small" />
+      <Input placeholder="middle" size="middle" />
+      <Input placeholder="large" size="large" />
+    </Fragment>
+  );
 }
