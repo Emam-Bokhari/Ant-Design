35cb9df28b876bc14be0c0148146ac9525e301e1
Moshfiqur Rahman <studentemam@gmail.com>
2024-10-25 12:29:28 +0600

    feat(Input): add addonBefore and addonAfter for URL input customization

--- src/components/learning/date-entry/InputComponent.jsx
+++ src/components/learning/date-entry/InputComponent.jsx
@@ -1,7 +1,23 @@
-import { Input } from "antd";
+import { Input, Select } from "antd";
+import { Option } from "antd/es/mentions";
 import { Fragment } from "react";
 
 export default function InputComponent() {
+  const seleftBefore = (
+    <Select defaultValue="http://">
+      <Option value="http://">http://</Option>
+      <Option value="https://">https://</Option>
+    </Select>
+  );
+  const selectAfter = (
+    <Select defaultValue=".com">
+      <Option value=".com">.com</Option>
+      <Option value=".org">.org</Option>
+      <Option value=".net">.net</Option>
+      <Option value=".xyz">.xyz</Option>
+      <Option value=".gov">.gov</Option>
+    </Select>
+  );
   return (
     <Fragment>
       <Input placeholder="Enter Your Name " />
@@ -13,6 +29,22 @@ export default function InputComponent() {
       <Input placeholder="outlined" variant="outlined" />
       <Input placeholder="filled" variant="filled" />
       <Input placeholder="borderless" variant="borderless" />
+      {/* pre post tab */}
+      <Input
+        addonBefore="https://"
+        addonAfter=".com"
+        placeholder="Enter Your Site URL"
+      />
+      <Input
+        addonBefore={
+          <Select defaultValue="http://">
+            <Option value="https://">https://</Option>
+            <option value="http://">http://</option>
+          </Select>
+        }
+      />
+      <Input addonBefore={seleftBefore} />
+      <Input addonBefore={seleftBefore}  addonAfter={selectAfter} />
     </Fragment>
   );
 }
